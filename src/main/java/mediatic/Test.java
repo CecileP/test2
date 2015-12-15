cf78e85b0300d433850e243725ada34991141302
CecileP <cecile.peladan@gmail.com>
2015-12-15 10:38:46 +0100

    mise en place du .gitignore

---
+++ src/main/java/mediatic/Test.java
@@ -0,0 +1,24 @@
+package mediatic;
+
+
+
+public class Test {
+
+	public static void test() {
+		myMethod();
+		
+		
+		
+		ArrayList
+	}
+
+	private static void myMethod() {
+		int i = 9;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Strings.isNullOrEmpty(""));
+	}
+
+}
